159fedfd779bd210fd33f7ccbc0825500a9dfac3
Marvellous Akib <marvellous.akib@gmail.com>
2024-02-29 20:04:32 +0000

    added functionality to add new flights, display all flights, and display specific flights

---
+++ start_point/src/main/java/com/example/airline_api/services/FlightService.java
@@ -0,0 +1,36 @@
+package com.example.airline_api.services;
+
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.repositories.FlightRepository;
+import com.example.airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class FlightService {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+
+//    method for displaying all flights
+    public List<Flight> findAllFlights(){
+        return flightRepository.findAll();
+    }
+
+//    method for displaying a specific flight
+    public Flight getAllFlights(Long id){
+        return flightRepository.findById(id).get();
+    }
+
+
+//    method for adding details of a new flight
+    public void saveFlight(Flight flight){
+        flightRepository.save(flight);
+    }
+
+}
